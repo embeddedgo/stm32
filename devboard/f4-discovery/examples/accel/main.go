541f764e795941e67559ff7be2e48f96ab4770f1
Michał Derkacz <michal@Lnet.pl>
2019-12-09 02:34:37 +0100

    f4-discovery/examples: improved accel example

--- devboard/f4-discovery/examples/accel/main.go
+++ devboard/f4-discovery/examples/accel/main.go
@@ -1,20 +1,65 @@
-// Copyright 2019 Michal Derkacz. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// This example shows how to use SPI peripheral to read the on-board LIS3DSH
-// accelerometer.
 package main
 
 import (
 	"embedded/rtos"
 
+	"github.com/embeddedgo/stm32/hal/exti"
 	"github.com/embeddedgo/stm32/hal/gpio"
+	"github.com/embeddedgo/stm32/hal/irq"
 	"github.com/embeddedgo/stm32/hal/spi"
 	"github.com/embeddedgo/stm32/hal/spi/spi1"
 
 	_ "github.com/embeddedgo/stm32/devboard/f4-discovery/board/init"
 )
+
+const (
+	CTRL_REG4 = 0x20
+	CTRL_REG3 = 0x23
+	OUT_X_L   = 0x28
+)
+
+var accel *LIS3DSH
+
+type LIS3DSH struct {
+	d   *spi.Driver
+	cs  gpio.Pin
+	dri exti.Lines
+	drn rtos.Note
+}
+
+func NewLIS3DSH(d *spi.Driver, cs gpio.Pin, dri exti.Lines) *LIS3DSH {
+	return &LIS3DSH{d: d, cs: cs, dri: dri}
+}
+
+func (a *LIS3DSH) Init() {
+	rtos.Nanosleep(10e6 - rtos.Nanotime())
+	a.cs.Clear()
+	a.d.WriteRead([]byte{CTRL_REG4, 0x67}, nil)
+	a.cs.Set()
+	a.cs.Clear()
+	a.d.WriteRead([]byte{CTRL_REG3, 0xC8}, nil)
+	a.cs.Set()
+}
+
+func (a *LIS3DSH) ReadXYZ() (x, y, z int) {
+	a.drn.Clear()
+	a.dri.EnableIRQ()
+	a.drn.Sleep(-1)
+	buf := [1 + 6]byte{1<<7 | OUT_X_L}
+	a.cs.Clear()
+	a.d.WriteRead(buf[:1], buf[:])
+	a.cs.Set()
+	x = int(int16(buf[1])|int16(buf[2])<<8) * 2000 / 32768
+	y = int(int16(buf[3])|int16(buf[4])<<8) * 2000 / 32768
+	z = int(int16(buf[5])|int16(buf[6])<<8) * 2000 / 32768
+	return
+}
+
+func (a *LIS3DSH) ISR() {
+	a.dri.DisableIRQ()
+	a.dri.ClearPending()
+	a.drn.Wakeup()
+}
 
 func main() {
 	// Allocate GPIO pins
@@ -27,13 +72,22 @@ func main() {
 
 	pe := gpio.E()
 	pe.EnableClock(true)
+	dr := pe.Pin(0)
 	cs := pe.Pin(3)
 
 	// Configure SPI pins
 
 	spi1.UsePinsMaster(sck, mosi, miso)
 	cs.Set() // CS active state is low
-	cs.Setup(&gpio.Config{Mode: gpio.Out})
+	cs.Setup(&gpio.Config{Mode: gpio.Out, Speed: gpio.High})
+
+	// Configure EXTI
+
+	dr.Setup(&gpio.Config{Mode: gpio.In})
+	dri := exti.Lines(1 << dr.Index())
+	dri.Connect(dr.Port())
+	dri.EnableRiseTrig()
+	irq.EXTI0.Enable(rtos.IntPrioLow)
 
 	// Configure and enable SPI
 
@@ -42,47 +96,44 @@ func main() {
 	d.SetWordSize(8)
 	d.Enable()
 
-	// LIS3DSH
+	// Reading acceleration data using DRDY interrupt
 
-	const (
-		READ      = 1 << 7
-		OFF_X     = 0x10
-		CTRL_REG4 = 0x20
-		OUT_X_L   = 0x28
-	)
-
-	buf := make([]byte, 7)
-
-	// initialize
-	buf[0] = CTRL_REG4
-	buf[1] = 0x57 // 50 Hz, block data update, all axis enabled
-	cs.Clear()
-	d.WriteRead(buf[:2], nil)
-	cs.Set()
-
-	// read correction offsets
-	buf[0] = READ | OFF_X
-	cs.Clear()
-	d.WriteRead(buf[:4], nil)
-	cs.Set()
-	ox := int32(int8(buf[1]))
-	oy := int32(int8(buf[2]))
-	oz := int32(int8(buf[3]))
-	println("correction offsets:", ox, oy, oz)
-
-	// print values in G/1000 units
-	for i := 0; ; i++ {
-		buf[0] = READ | OUT_X_L
-		cs.Clear()
-		d.WriteRead(buf, buf)
-		cs.Set()
-		x := int32(int16(buf[1])|int16(buf[2])<<8) + ox
-		y := int32(int16(buf[3])|int16(buf[4])<<8) + oy
-		z := int32(int16(buf[5])|int16(buf[6])<<8) + oz
-		x = x * 2000 / 32768
-		y = y * 2000 / 32768
-		z = z * 2000 / 32768
-		println(x, y, z)
-		rtos.Nanosleep(1e8)
+	accel = NewLIS3DSH(d, cs, dri)
+	accel.Init()
+	for {
+		show(accel.ReadXYZ())
 	}
 }
+
+func show(x, y, z int) {
+	gauge("x", x)
+	gauge("y", y)
+	gauge("z", z)
+	print("+-----------------------------------------+\n")
+	rtos.Nanosleep(100e6)
+}
+
+func gauge(name string, v int) {
+	const bar = "--------------------"
+	const spc = "                    "
+	v /= 100
+	print("|")
+	switch {
+	case v > 0:
+		if v > 20 {
+			v = 20
+		}
+		print(spc, name, bar[:v], spc[v:])
+	case v < 0:
+		if v < -20 {
+			v = -20
+		}
+		print(spc[:20+v], bar[20+v:], name, spc)
+	default: // v == 0:
+		print(spc, name, spc)
+	}
+	print("|\n")
+}
+
+//go:interrupthandler
+func EXTI0_Handler() { accel.ISR() }
