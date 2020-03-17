5495002093be9374a36dfc35251a78e293d5afc9
Michał Derkacz <michal@Lnet.pl>
2020-03-17 01:21:16 +0100

    examples: add eve examples

---
+++ devboard/f4-discovery/examples/evetest/main.go
@@ -0,0 +1,80 @@
+// Copyright 2020 Michal Derkacz. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"embedded/rtos"
+
+	"github.com/embeddedgo/display/eve"
+	"github.com/embeddedgo/stm32/dci/evedci"
+	"github.com/embeddedgo/stm32/hal/exti"
+	"github.com/embeddedgo/stm32/hal/gpio"
+	"github.com/embeddedgo/stm32/hal/irq"
+	"github.com/embeddedgo/stm32/hal/spi"
+	"github.com/embeddedgo/stm32/hal/spi/spi2"
+
+	"github.com/embeddedgo/display/eve/examples/gameduino/nightstrike"
+
+	_ "github.com/embeddedgo/stm32/devboard/f4-discovery/board/init"
+)
+
+var eveInt rtos.Note
+
+func main() {
+	// Allocate GPIO pins
+
+	pb := gpio.B()
+	pb.EnableClock(true)
+	intn := pb.Pin(1)
+	pdn := pb.Pin(11)
+	csn := pb.Pin(12)
+	sck := pb.Pin(13)
+	miso := pb.Pin(14)
+	mosi := pb.Pin(15)
+
+	// EVE driver
+
+	evedci.UseIntPin(intn)
+	irq.EXTI1.Enable(rtos.IntPrioLowest)
+
+	spi2.UsePinMaster(spi.NSS, csn)
+	spi2.UsePinMaster(spi.SCK, sck)
+	spi2.UsePinMaster(spi.MOSI, mosi)
+	spi2.UsePinMaster(spi.MISO, miso)
+
+	lcd := eve.NewDriver(evedci.NewSPI(spi2.Driver(), pdn), 256)
+	lcd.SetNote(&eveInt)
+
+	////
+
+	// Dithering causes distortion of vertical gradients on my FT811CB-HY50HD
+	// (horizontal darker lines appear). The effect is most visible if both
+	// Dither and Spread are set at the same time (default after reset).
+	// Reducing PCLK to 15 MHz eliminates this problem but slows down the
+	// display to 30 FPS.
+	dispCfg := &eve.Default800x480
+	//dispCfg.ClkMHz = 15
+	//dispCfg := &eve.Default480x272
+	checkErr(lcd.Init(dispCfg, &eve.Config{Dither: 1}))
+
+	lcd.SetBacklight(96)
+
+	nightstrike.Run(lcd)
+}
+
+func checkErr(err error) {
+	if err == nil {
+		return
+	}
+	println(err.Error())
+	for {
+	}
+}
+
+//go:interrupthandler
+func EXTI1_Handler() {
+	exti.L1.ClearPending()
+	eveInt.Wakeup()
+}
---
+++ devboard/nucleo-l476rg/examples/evetest/main.go
@@ -0,0 +1,78 @@
+// Copyright 2020 Michal Derkacz. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"embedded/rtos"
+
+	"github.com/embeddedgo/display/eve"
+	"github.com/embeddedgo/stm32/dci/evedci"
+	"github.com/embeddedgo/stm32/hal/exti"
+	"github.com/embeddedgo/stm32/hal/gpio"
+	"github.com/embeddedgo/stm32/hal/irq"
+	"github.com/embeddedgo/stm32/hal/spi"
+	"github.com/embeddedgo/stm32/hal/spi/spi2"
+
+	_ "github.com/embeddedgo/stm32/devboard/nucleo-l476rg/board/init"
+
+	"github.com/embeddedgo/display/eve/examples/evetest"
+)
+
+var eveInt rtos.Note
+
+func main() {
+	// Allocate GPIO pins
+
+	pb := gpio.B()
+	pb.EnableClock(true)
+	intn := pb.Pin(1)
+	pdn := pb.Pin(11)
+	csn := pb.Pin(12)
+	sck := pb.Pin(13)
+	miso := pb.Pin(14)
+	mosi := pb.Pin(15)
+
+	// EVE driver
+
+	evedci.UseIntPin(intn)
+	irq.EXTI1.Enable(rtos.IntPrioLowest)
+
+	spi2.UsePinMaster(spi.NSS, csn)
+	spi2.UsePinMaster(spi.SCK, sck)
+	spi2.UsePinMaster(spi.MOSI, mosi)
+	spi2.UsePinMaster(spi.MISO, miso)
+
+	lcd := eve.NewDriver(evedci.NewSPI(spi2.Driver(), pdn), 256)
+	lcd.SetNote(&eveInt)
+
+	// Dithering causes distortion of vertical gradients on my FT811CB-HY50HD
+	// (horizontal darker lines appear). The effect is most visible if both
+	// Dither and Spread are set at the same time (default after reset).
+	// Reducing PCLK to 15 MHz eliminates this problem but slows down the
+	// display to 30 FPS.
+	
+	//dispCfg := &eve.Default800x480
+	//dispCfg.ClkMHz = 15
+	
+	dispCfg := &eve.Default480x272
+	
+	checkErr(lcd.Init(dispCfg, nil))
+	checkErr(evetest.Run(lcd))
+}
+
+func checkErr(err error) {
+	if err == nil {
+		return
+	}
+	println(err.Error())
+	for {
+	}
+}
+
+//go:interrupthandler
+func EXTI1_Handler() {
+	exti.L1.ClearPending()
+	eveInt.Wakeup()
+}
