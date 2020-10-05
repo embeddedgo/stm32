c0bae63996e703eeb0a53e0e32cadc5105ca79fe
Michał Derkacz <michal@Lnet.pl>
2020-10-06 00:57:56 +0200

    devebox-h743: new board

---
+++ devboard/devebox-h743/examples/debug-oocd.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+INTERFACE=stlink
+TARGET=stm32h7x_dual_bank
+
+. ../../../../../scripts/debug-oocd.sh
---
+++ devboard/devebox-h743/examples/load-oocd.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+INTERFACE=stlink
+TARGET=stm32h7x_dual_bank
+TRACECLKIN=400000000
+
+. ../../../../../scripts/load-oocd.sh
