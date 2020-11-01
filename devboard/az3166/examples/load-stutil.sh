91ed2ed7c0ff00d9e35b082b4cc28de5e6051e84
Michał Derkacz <michal@Lnet.pl>
2020-11-01 22:43:20 +0100

    az3166/examples: update stutil scripts

--- devboard/az3166/examples/load-stutil.sh
+++ devboard/az3166/examples/load-stutil.sh
@@ -1,8 +1,1 @@
-#!/bin/sh
-
-set -e
-
-name=$(basename $(pwd))
-arm-none-eabi-objcopy -O binary $name.elf $name.bin
-st-flash --reset write $name.bin 0x8000000
-rm $name.bin
+../../../../scripts/load-stutil.sh
