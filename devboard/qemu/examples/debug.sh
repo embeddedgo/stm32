aff26e8274834facad29102456c7b4cb683ecbf6
Michał Derkacz <michal@Lnet.pl>
2024-07-29 15:36:53 +0200

    devboard: remove qemu

--- devboard/qemu/examples/debug.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-name=$(basename $(pwd))
-
-qemu-system-arm -machine netduinoplus2 -cpu cortex-m4 -gdb tcp::3333 -S -nographic -monitor none -serial none --semihosting-config enable=on,target=native,userspace=on -kernel $name.elf
--- devboard/qemu/examples/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-name=$(basename $(pwd))
-
-# -serial stdio
-
-qemu-system-arm -machine netduinoplus2 -cpu cortex-m4 -nographic -monitor none -serial none --semihosting-config enable=on,target=native,userspace=on -kernel $name.elf
