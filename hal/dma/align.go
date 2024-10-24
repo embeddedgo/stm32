cea9da6e3dd555dd19476232a310b574a0f968b4
Michał Derkacz <michal@Lnet.pl>
2024-10-24 23:22:32 +0200

    hal/dma: DMA memory alignment

---
+++ hal/dma/align.go
@@ -0,0 +1,47 @@
+// Copyright 2024 The Embedded Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package dma
+
+import "unsafe"
+
+// An MemAlign is the prefered memory alignment for DMA operations. It's always
+// power of 2 and defined in such a way that a fully MemAlign aligned (top and
+// bottom) memory region corresponds to the complete data cache lines.
+const MemAlign = cacheLineSize
+
+// A CacheMaint indicates whether DMA requires cache maintenance.
+const CacheMaint = cacheMaint
+
+// AlignOffsets calculatest the start and end offsets to the MemAlign aligned
+// portion of the memory described by ptr and size.
+func AlignOffsets(ptr unsafe.Pointer, size uintptr) (start, end uintptr) {
+	const alignMask = MemAlign - 1
+	p := uintptr(ptr)
+	start = -p & alignMask
+	end = size - (p+size)&alignMask
+	return
+}
+
+func alloc(size uintptr) unsafe.Pointer {
+	size = (size + (cacheLineSize - 1)) &^ (cacheLineSize - 1)
+	size += cacheLineSize // extra space for address alignment
+	buf := make([]byte, size)
+	addr := uintptr(unsafe.Pointer(&buf[0]))
+	addr = (addr + (cacheLineSize - 1)) &^ (cacheLineSize - 1)
+	return unsafe.Pointer(addr)
+}
+
+// New works like new(T) but guarantees that the allocated variable has the
+// prefered DMA alignment (see MemAlign).
+func New[T any]() (ptr *T) {
+	return (*T)(alloc(unsafe.Sizeof(*ptr)))
+}
+
+// MakeSlice works like make([]T, len, cap) but guarantees that the returned
+// slice has the prefered DMA alignment (see MemAlign).
+func MakeSlice[T any](len, cap int) (slice []T) {
+	ptr := alloc(unsafe.Sizeof(slice[0]) * uintptr(cap))
+	return unsafe.Slice((*T)(ptr), cap)[:len]
+}
