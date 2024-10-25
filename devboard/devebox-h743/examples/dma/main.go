60a34355c23e99ee162ff6c17343bc051f70a1d9
Michał Derkacz <michal@Lnet.pl>
2024-10-25 16:58:26 +0200

    devebox-h743/examples: simple DMA example

---
+++ devboard/devebox-h743/examples/dma/main.go
@@ -0,0 +1,80 @@
+// Copyright 2024 The Embedded Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"embedded/rtos"
+	"time"
+	"unsafe"
+
+	"github.com/embeddedgo/stm32/hal/dma"
+	"github.com/embeddedgo/stm32/hal/irq"
+
+	"github.com/embeddedgo/stm32/devboard/devebox-h743/board/leds"
+)
+
+var (
+	ch  dma.Channel
+	tce rtos.Note
+)
+
+func copyDMA(dst, src unsafe.Pointer, n int, mode dma.Mode) dma.Error {
+	tce.Clear()
+	ch.Setup(dma.MTM | dma.IncP | dma.IncM | mode)
+	ch.SetWordSize(4, 4)
+	ch.SetLen(n)
+	ch.SetAddrP(src)
+	ch.SetAddrM(dst)
+	ch.Clear(dma.EvAll, dma.ErrAll)
+	ch.EnableIRQ(dma.Complete, dma.ErrAll)
+	ch.Enable()
+	tce.Sleep(-1)
+	_, err := ch.Status()
+	return err
+}
+
+func main() {
+	d := dma.DMA(2)
+	d.EnableClock(true)
+	ch = d.Channel(0, 0)
+	irq.DMA2_STR0.Enable(rtos.IntPrioLow, 0)
+
+	n := 4000
+	src := dma.MakeSlice[uint32](n, n)
+	dst := dma.MakeSlice[uint32](n, n)
+	srcAddr := unsafe.Pointer(&src[0])
+	dstAddr := unsafe.Pointer(&dst[0])
+
+	for i := range src {
+		src[i] = uint32(i)
+	}
+	rtos.CacheMaint(rtos.DCacheFlush, srcAddr, n*4)
+	rtos.CacheMaint(rtos.DCacheFlushInval, dstAddr, n*4)
+
+	delay := time.Second
+	if copyDMA(dstAddr, srcAddr, n, dma.FT4|dma.PB4|dma.MB4) != 0 {
+		delay /= 4
+	} else {
+		for i, v := range src {
+			if v != dst[i] {
+				delay /= 4
+				break
+			}
+		}
+	}
+
+	for {
+		leds.User.Toggle()
+		time.Sleep(delay)
+	}
+
+}
+
+//go:interrupthandler
+func DMA2_STR0_Handler() {
+	ch.DisableIRQ(dma.EvAll, dma.ErrAll)
+	ch.Disable()
+	tce.Wakeup()
+}
