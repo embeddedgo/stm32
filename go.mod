c02e16d3c3cd59eb8bf236fa3215bc05db422afa
Michał Derkacz <michal@Lnet.pl>
2025-01-09 15:51:05 +0100

    hal/spih: work in progress on new SPI driver for H7

--- go.mod
+++ go.mod
@@ -8,3 +8,5 @@ require (
 	github.com/embeddedgo/fs v0.1.3
 	github.com/embeddedgo/onewire v0.0.2
 )
+
+//GOTARGET=stm32h7x3
--- hal/spih/bits.go
+++ hal/spih/bits.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package spi
+package spih
 
 const (
 	SPE      CR1 = 0x01 << 0  //+ Serial Peripheral Enable
